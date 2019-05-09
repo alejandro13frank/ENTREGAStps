ac3d9a8a4c97f2fb0c162144e146a139f5ec0965
alumno <alumno@LAB00PC00>
2019-05-08 21:01:22 -0300

    Practica parcial autores

---
+++ practica parcial 1/socio.h
@@ -0,0 +1,38 @@
+#ifndef SOCIO_H_INCLUDED
+#define SOCIO_H_INCLUDED
+
+#define TEXT_SIZE 20
+
+typedef struct
+{
+    int idUnico;
+    int isEmpty;
+    //-----------------
+    char nombre[TEXT_SIZE];//varString
+    //int varInt;//varint
+    //float varFloat;//varfloat
+    char sexo;//varchar
+    char apellido[TEXT_SIZE];//varlonstring
+    char telefono[TEXT_SIZE];
+    char email[TEXT_SIZE];
+    int dia;
+    int mes;
+    int ano;
+}Socio;
+
+
+#endif // SOCIO_H_INCLUDED
+
+int socio_Inicializar(Socio array[], int size);                                    //cambiar socio
+int socio_buscarEmpty(Socio array[], int size, int* posicion);                    //cambiar socio
+int socio_buscarID(Socio array[], int size, int valorBuscado, int* posicion);                    //cambiar socio
+int socio_buscarInt(Socio array[], int size, int valorBuscado, int* posicion);                    //cambiar socio
+int socio_buscarString(Socio array[], int size, char* valorBuscado, int* indice);                    //cambiar socio
+int socio_alta(Socio array[], int size, int* contadorID);                          //cambiar socio
+int socio_baja(Socio array[], int sizeArray);                                      //cambiar socio
+int socio_bajaValorRepetidoInt(Socio array[], int sizeArray, int valorBuscado);
+int socio_modificar(Socio array[], int sizeArray);                                //cambiar socio
+int socio_ordenarPorString(Socio array[],int size);                              //cambiar socio
+int socio_listar(Socio array[], int size);                      //cambiar socio
+
+
